edb767e55d97bd821d7d77dbca9a1332a093b03c
syp <sy95_055@naver.com>
2022-08-27 19:54:38 +0900

    feat: Carousel 컴포넌트 추가 / 기능 구현

---
+++ src/components/Carousel.js
@@ -0,0 +1,103 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import styled from 'styled-components';
+import { useEffect, useRef } from 'react';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+};
+
+const StyledSlider = styled(Slider)`
+  .slick-arrow .slick-prev {
+  }
+  .slick-arrow .slick-next {
+  }
+
+  .slick-dots {
+    bottom: 40px;
+
+    li {
+      width: 2px;
+      height: 2px;
+      margin: 0 7px;
+
+      button {
+        width: 2px;
+        height: 2px;
+      }
+      button:before {
+        width: 2px;
+        height: 2px;
+        color: white;
+      }
+    }
+  }
+`;
+
+const SliderImage = styled.div`
+  position: relative;
+  width: 100vw;
+  min-height: 400px;
+
+  img {
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+    object-position: center center;
+  }
+`;
+
+export default function Carousel() {
+  const customSliderRef = useRef();
+
+  const SlickNext = () => {
+    customSliderRef.current.slickNext();
+  };
+
+  const SlickPrev = () => {
+    customSliderRef.current.slickPrev();
+  };
+
+  const SlickGoTo = (num, e) => {
+    customSliderRef.current.slickGoTo(num);
+  };
+
+  return (
+    <>
+      <StyledSlider {...settings} ref={customSliderRef}>
+        <SliderImage>
+          <img src="/store-main-image/image1.webp" alt="" />
+        </SliderImage>
+        <SliderImage>
+          <img src="/store-main-image/image2.webp" alt="" />
+        </SliderImage>
+        <SliderImage>
+          <img src="/store-main-image/image3.webp" alt="" />
+        </SliderImage>
+        <SliderImage>
+          <img src="/store-main-image/image4.webp" alt="" />
+        </SliderImage>
+        <SliderImage>
+          <img src="/store-main-image/image5.webp" alt="" />
+        </SliderImage>
+      </StyledSlider>
+      <div>
+        <button onClick={e => SlickGoTo(0)}>주말특가</button>
+        <button onClick={e => SlickGoTo(1)}>베스트 브랜드</button>
+        <button onClick={e => SlickGoTo(2)}>OI푸드위크</button>
+        <button onClick={e => SlickGoTo(3)}>LG전자 세일</button>
+        <button onClick={e => SlickGoTo(4)}>특가 찬스</button>
+        <button onClick={SlickPrev}>Prev</button>
+        <button onClick={SlickNext}>Next</button>
+      </div>
+    </>
+  );
+}
--- src/pages/store/index.js
+++ src/pages/store/index.js
@@ -1,9 +1,13 @@
+import Carousel from 'components/Carousel';
+import Image from 'next/image';
+
 import React from 'react';
 
 export default function Store() {
   return (
     <>
       <h1 className="title">Store</h1>
+      <Carousel />
     </>
   );
 }
