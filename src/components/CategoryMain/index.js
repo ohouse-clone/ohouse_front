30e42abccda59cc46779cbc1b2a00595cf5d1739
syp <sy95_055@naver.com>
2022-09-24 14:47:11 +0900

    feat : category 기능 제작

--- src/components/CategoryMain/index.js
+++ src/components/CategoryMain/index.js
@@ -1,11 +1,11 @@
 import StoreinfiniteProducts from 'components/StoreMain/StoreInfiniteProducts';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const furnitureCategories = [
-  { title: '침대' },
-  { title: '매트리스, 토퍼' },
-  { title: '소파' },
+  { title: '침대', hash: 1 },
+  { title: '매트리스, 토퍼', hash: 2 },
+  { title: '소파', hash: 3 },
 ];
 
 const LayoutWrapper = styled.div`
@@ -23,30 +23,96 @@ const CategoryWrapper = styled.div`
 `;
 
 export default function CategoryMain() {
-  const [categoryClick, setCategoryClick] = useState(false);
-  const categoryClickToggle = () => {
-    setCategoryClick(prev => !prev);
+  const [categoryClick, setCategoryClick] = useState('가구');
+  const [fetchApiAddress, setFetchApiAddress] = useState('가구');
+  const [data, setData] = useState();
+
+  const categoryClickToggle = e => {
+    setCategoryClick(e.dataset);
+    setFetchApiAddress(e.dataset);
   };
+
+  useEffect(() => {
+    fetch(fetchApiAddress).then(res => setData(res));
+  }, [fetchApiAddress]);
+
+  // 1. 클릭 이벤트시
+  //  타이틀이 맨 위로 올라와야함 / categories.title 을 map해서 리스트화 시켜야 함 /
+  // hash를 전달해서 주소를 바꿔야 함 / filter를 map해서 필터를 구현 /
+  // content가 있을시 슬라이더 등 나타나게 / fetch api를 해당 카테고리의 api로 변경
+
   return (
     <LayoutWrapper>
       <Wrapper>
-        <CategoryWrapper>
-          categories.map 클릭 시 맨위로 올리는 작업 다른 리스트가 다 닫히는 것.
-          {/* 카테고리 한개가 클릭 되었다면 제일 위로 올리고 나머지는 끄기. 
-      recoil로 관리하면 될 듯. */}
-          <h1 onClick={categoryClickToggle}>가구</h1>
-          {categoryClick && (
-            <>
-              {furnitureCategories.map((data, i) => {
-                return <div key={i}>{data.title}</div>;
-              })}
-            </>
-          )}
-        </CategoryWrapper>
+        <>
+          <CategoryWrapper>
+            data map.categories.title
+            <div>현재 fetch : {fetchApiAddress}</div>
+            <br />
+            <br />
+            <h1 onClick={() => setFetchApiAddress(categoryClick)}>
+              {categoryClick}
+            </h1>
+            <ul>
+              {categoryClick === '가구' && (
+                <>
+                  {furnitureCategories.map((data, i) => {
+                    return (
+                      <li
+                        key={i}
+                        onClick={() => setFetchApiAddress('가구 ' + data.hash)}
+                      >
+                        {data.title}
+                      </li>
+                    );
+                  })}
+                </>
+              )}
+            </ul>
+            <br />
+            <br />
+            <ul>
+              {categoryClick === '가구' || (
+                <li
+                  onClick={() => {
+                    setCategoryClick('가구');
+                    setFetchApiAddress('가구');
+                  }}
+                >
+                  가구
+                </li>
+              )}
+
+              {categoryClick === '패브릭' || (
+                <li
+                  onClick={() => {
+                    setCategoryClick('패브릭');
+                    setFetchApiAddress('패브릭');
+                  }}
+                >
+                  패브릭
+                </li>
+              )}
+
+              {categoryClick === '조명' || (
+                <li
+                  onClick={() => {
+                    setCategoryClick('조명');
+                    setFetchApiAddress('조명');
+                  }}
+                >
+                  조명
+                </li>
+              )}
+            </ul>
+          </CategoryWrapper>
+        </>
         <div>
           <h1>가구</h1>
           <div>슬라이더</div>
-          <div>공간별가구 찾기 </div>
+          {/* <div>{data.property_groups.map}</div> */}
+
+          <div>공간별가구 찾기</div>
           <div>필터</div>
           <StoreinfiniteProducts />
         </div>
